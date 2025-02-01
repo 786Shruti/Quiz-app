71e4ddc9f9e5f46bf21aee9549dcc47740429d4d
Shruti Satish Pol <143900541+786Shruti@users.noreply.github.com>
2025-02-02 00:48:08 +0530

    Update Quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,7 +4,7 @@ import Summary from "./Summary"; // Ensure the path is correct
 
 import "./Quiz.css";
 
-const API_URL = "http://localhost:5000/api/quiz";
+const API_URL = "https://quiz-app-go3x.onrender.com";
 
 const Quiz = () => {
   const [questions, setQuestions] = useState([]);
